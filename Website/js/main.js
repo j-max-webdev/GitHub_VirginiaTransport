2dafaab92318a6695016ecb8c1493dbadd923fbf
j-max <jmax.barry@gmail.com>
2017-01-06 09:02:49 -0600

    With padding in navbar

--- Website/js/main.js
+++ Website/js/main.js
@@ -29,5 +29,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 10000); // Change image every 2 seconds
+    setTimeout(carousel, 30000); // Change image every 2 seconds
    }
