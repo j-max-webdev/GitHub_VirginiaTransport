98004a976a4a2e487bf81f34be43b7599378dece
j-max <jmax.barry@gmail.com>
2017-01-23 14:58:27 -0600

    Before No Red

--- Website/js/main.js
+++ Website/js/main.js
@@ -31,5 +31,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 5000); 
+    setTimeout(carousel, 7000); 
    }
